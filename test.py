f65936e9bdfd3c8a24661172027d5c700416aa66
Gehad Almolla <gehad.almolla@gmail.com>
2025-11-02 15:57:19 +0200

    fixing again

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 import unittest
-from Power import Power   # import it!
 
 class Test_Power(unittest.TestCase):
     def setUp(self):
+        # This method is run before each test, useful for setup code.
         self.pow = Power()
-
     def test_add(self):
+        # Test addition functionality
         result = self.pow.pow(3, 3)
         self.assertEqual(result, 9)
-
-if __name__ == "__main__":
-    unittest.main()
